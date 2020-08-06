8adac26e6841e64f76f247193c45c3f6d01c2ec4
hym2020 <66545285+hym2020@users.noreply.github.com>
2020-08-06 18:26:16 +0800

    Update download_driver.js

--- src/download_driver.js
+++ src/download_driver.js
@@ -5,7 +5,7 @@ import JSZip from 'jszip'
 
 
 const downloader = (() => {
-	const UA = { 'Content-Type': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
+	const UA = { 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
 
 	let driverVersion
 
